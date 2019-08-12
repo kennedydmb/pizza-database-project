908bc0e6b9e0d59f756a67ec95d9ce6647bc7925
Ubuntu <ubuntu@ip-172-31-36-103.ec2.internal>
2019-08-12 16:08:54 +0000

    Installation for flask_pymongo and bson added

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-    
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'pizza_manager'
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
