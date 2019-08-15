f2404247c6c32694a68cf80eb986a8fa4ab92bea
Ubuntu <ubuntu@ip-172-31-36-103.ec2.internal>
2019-08-15 14:45:32 +0000

    Added add pizza and insert pizza routes. And added form to addpizza.html, user can select sauce type from drop down

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ meat = mongo.db.ingredients.find_one({'meat' : {'$exists': True}})
 veg = mongo.db.ingredients.find_one({'veg' : {'$exists': True}})
 sauce = mongo.db.ingredients.find_one({'sauce' : {'$exists': True}})
 
-print(sauce)
-print(veg)
                             
 @app.route('/')
 @app.route('/get_pizzas')
@@ -24,7 +22,20 @@ def get_pizzas():
                            meat = meat,
                            veg = veg,
                            sauce = sauce)
-    
+
+@app.route('/add_pizza')
+def add_pizza():
+    return render_template("addpizza.html", 
+                           meat = meat,
+                           veg = veg,
+                           sauce = sauce)
+                           
+@app.route('/insert_pizza', methods=['POST'])
+def insert_pizza():
+    pizzas =  mongo.db.pizzas
+    pizzas.insert_one(request.form.to_dict())
+    return redirect(url_for('get_pizzas'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
